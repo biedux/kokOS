e4dad7b6fd30564121992dac68c79f60608885eb
ElStempel <steplewski.aleks@gmail.com>
2019-12-12 13:44:06 +0100

    chary i sugestie ciechana

--- .idea/memory.java
+++ .idea/memory.java
@@ -1,12 +1,12 @@
 package put.poznan.pl;
-
+//JEDZIEMY CHARAMI
 
         public class Memory {
-            private static byte[] memory = new byte[256];
+            private static char[] memory = new char[256];
 
 
             //ZAPIS POJEDYNCZEGO BAJTU
-            void write(byte data, int i){
+            void write(char data, int i){
                 try {
                     memory[i] = data;
                 }
@@ -17,7 +17,7 @@ package put.poznan.pl;
 
 
             //ODCZYT POJEDYNCZEGO BAJTU
-            public byte read(int i) {
+            public char read(int i) {
                 try {
                     return memory[i];
                 }
@@ -29,12 +29,12 @@ package put.poznan.pl;
 
 
             //tu bedzie ZAPIS STRONICY W RAMCE
-            public static boolean writeFrame(Vector<Byte> data, int frame){
+            public static boolean writeFrame(Vector<Char> data, int frame){
                 if ((frame < 0) || (frame > 15)){
                     return false;
                 }
                 for (int i = 0; i < data.size(); i++){
-                    memory[frame * 16 + i] = memory.get(i);
+                    memory[frame * 16 + i] = data.get(i);
                 }
                 return true;
             }
