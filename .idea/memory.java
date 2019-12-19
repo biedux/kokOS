ed428cb1cd0ad26b79bb0a8562cbea650fc19d4f
ElStempel <steplewski.aleks@gmail.com>
2019-12-19 12:36:09 +0100

    aktualnie czekam na damianowe, a tak to git

--- .idea/memory.java
+++ .idea/memory.java
@@ -1,82 +1,97 @@
-package put.poznan.pl;
-//JEDZIEMY CHARAMI
+package memory;
 
-        public class Memory {
-            private static char[] memory = new char[256];
+import java.util.Vector;
 
-
-            //ZAPIS POJEDYNCZEGO BAJTU
-            void write(char data, int i){
-                try {
-                    memory[i] = data;
-                }
-                catch (Exception e) {
-                    System.out.println(e.getMessage());
-                }
-            }
+public class memory {
+    private static char[] memory = new char[256];
 
 
-            //ODCZYT POJEDYNCZEGO BAJTU
-            public char read(int i) {
-                try {
-                    return memory[i];
-                }
-                catch (Exception e) {
-                    System.out.println(e.getMessage());
-                    return 0;
-                }
-            }
-
-
-            //tu bedzie ZAPIS STRONICY W RAMCE
-            public static boolean writeFrame(Vector<Char> data, int frame){
-                if ((frame < 0) || (frame > 15)){
-                    return false;
-                }
-                for (int i = 0; i < data.size(); i++){
-                    memory[frame * 16 + i] = data[i];
-                }
-                return true;
-            }
-
-
-
-
-            //tu bedzie ODCZYT STRONICY Z RAMKI
-            public static Vector<Char> readFrame(int frame){
-                Vector<Char> odczyt = new Vector<Char>();
-                try {
-                    for (int i = 0; i < 16; i++){
-                        odczyt.add(memory[frame * 16 + i]);
-                    }
-                }
-                catch (Exception e) {
-                    System.out.println(e.getMessage());
-                }
-                return odczyt;
-            }
-
-
-
-            //tu bedzie WYSWIETLENIE ZAWARTOSCI RAMU (do debugowania)
-            public static void printRawRam() {
-                for (int i = 0; i < 16; i++) {
-                    System.out.println(memory[i]);
-                    for (int j = 0; j < 16; j++) {
-                        System.out.println(String.format(memory[i * 16 + j]));
-                    }
-                }
-                System.out.println("");
-
-                Utils.log("showing raw contents of RAM");
-            }
-
-
-
-            //tu bedzie ZAPIS BAJTU ZGODNIE Z TABLICA STRONIC
-
-
-
-
-            //tu bedzie ODCZYT BAJTU ZGODNIE Z TABLICA STRONIC
+    //ZAPIS POJEDYNCZEGO BAJTU
+    void write(char data, int i){
+        try {
+            memory[i] = data;
         }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    //ODCZYT POJEDYNCZEGO BAJTU
+    public char read(int i) {
+        try {
+            return memory[i];
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
+
+
+    //tu bedzie ZAPIS STRONICY W RAMCE
+    public static boolean writeFrame(Vector<Character> data, int frame){
+        if ((frame < 0) || (frame > 15)){
+            return false;
+        }
+        for (int i = 0; i < data.size(); i++){
+            memory[frame * 16 + i] = data.get(i);
+        }
+        return true;
+    }
+
+
+
+
+    //tu bedzie ODCZYT STRONICY Z RAMKI
+    public static Vector<Character> readFrame(int frame){
+        Vector<Character> odczyt = new Vector<Character>();
+        try {
+            for (int i = 0; i < 16; i++){
+                odczyt.add(memory[frame * 16 + i]);
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return odczyt;
+    }
+
+
+
+    //tu bedzie WYSWIETLENIE ZAWARTOSCI RAMU (do debugowania)
+    public static void printRawRam() {
+        for (int i = 0; i < 16; i++) {
+            System.out.println(memory[i]);
+            for (int j = 0; j < 16; j++) {
+                System.out.println(String.format(String.valueOf(memory[i * 16 + j])));
+            }
+        }
+        System.out.println("");
+    }
+
+
+
+    //tu bedzie ZAPIS BAJTU ZGODNIE Z TABLICA STRONIC
+    void writeToFrame(char data, int adress, int frame){
+        try {
+            memory[frame * 16 + (adress % 16)] = data;
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+
+    //tu bedzie ODCZYT BAJTU ZGODNIE Z TABLICA STRONIC
+    public char readFromFrame(int adress, int frame){
+        try {
+            return memory[frame * 16 + (adress % 16)];
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
+}
