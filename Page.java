04faf3277ef5fd5399d9a0484cedf20a81470395
Dami46 <56515672+Dami46@users.noreply.github.com>
2019-12-19 14:00:47 +0100

    Update Page.java

--- Page.java
+++ Page.java
@@ -1,14 +1,12 @@
 package Virtual;
 // pojedynczy wpis w tablicy stronic
-public class Page {
+class Page {
 
     boolean valid;
-    boolean odniesienia;
     int nrramki;
     Page()
     {
         valid = false;
-        odniesienia = false;
         nrramki = -1;
     }
 }
