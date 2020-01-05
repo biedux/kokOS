415a1f8e37e3c173e5e384c3b9dfb6a40d0844ef
Dodominionek <56478965+Dodominionek@users.noreply.github.com>
2020-01-05 20:32:30 +0100

    Create Page.java

---
+++ src/Process_Manager/Page.java
@@ -0,0 +1,13 @@
+package VirtualMemory;
+
+// pojedynczy wpis w tablicy stronic
+public class Page {
+
+    boolean valid;
+    int nrramki;
+    Page()
+    {
+        valid = false;
+        nrramki = -1;
+    }
+}
