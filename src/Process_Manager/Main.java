86c936a13746f93145acda9401bc1bdbb14dd756
Dodominionek <56478965+Dodominionek@users.noreply.github.com>
2020-01-07 19:02:56 +0100

    Update Main.java

--- src/Process_Manager/Main.java
+++ src/Process_Manager/Main.java
@@ -12,18 +12,6 @@ import VirtualMemory.VirtualMemory;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
-        //PM.showAllProcesses();
-        //System.out.println(" ");
-        //PM.kill(P3);
-        //PM.showAllProcesses();
-        //System.out.println(" ");
-
-        //PM.showAllProcesses();
-        //System.out.println(" ");
-        //PM.kill(P2);
-        //PCB P7=PM.fork(P3,"P7",120,"");
-        //PM.showAllProcesses();
-
         Process_Management PM=new Process_Management();
 
         /*
@@ -50,25 +38,20 @@ public class Main {
         close(pdesc[1]);
          */
 
-        //CP P1 P4 120 ""
-        PCB P1=PM.fork(PM.init, "P1",120,"test.txt");
-        //PCB P2=PM.fork(PM.init,"P2",120,"test2.txt");
-        PCB P3=PM.fork(PM.init,"P3",120,"");
-        PM.showTree(PM.init);
-        PM.scheduler.check();
-        System.out.println(PM.readFile(P1.getFileName()));
-        P1.setCounter(PM.readFile(P1.getFileName()).length()-1);
-        System.out.println(PM.readFile(P1.getFileName()).length() - 1 + " rozmiar");
-//        System.out.println(PM.readFile(P2.getFileName()));
-//        P2.setCounter(PM.readFile(P2.getFileName()).length()-1);
-//        System.out.println(PM.readFile(P2.getFileName()).length() - 1 + " rozmiar");
-        PM.showAllProcesses();
+        PCB P1=PM.fork(PM.init, "P1",120,"test1.txt");
+        PCB P2=PM.fork(PM.init,"P2",120,"test2.txt");
+        PCB P3=PM.fork(PM.init,"P3",120,"test3.txt");
+        PCB P4=PM.fork(PM.init,"P4",120,"test1.txt");
+        PCB P5=PM.fork(PM.init,"P5",120,"test1.txt");
         Memory.printRawRam();
-        //VirtualMemory.printPageFile(P1.getID());
-        //VirtualMemory.printPageTable(P1.getID());
-        //VirtualMemory.printQueue();
-//        VirtualMemory.printPageFile(P2.getID());
-//        VirtualMemory.printPageTable(P2.getID());
+        VirtualMemory.printPageFile(P1.getID());
+        VirtualMemory.printPageFile(P2.getID());
+        VirtualMemory.printPageFile(P3.getID());
+        VirtualMemory.printPageTable(P1.getID());
+
+
+
+
 //        int[] pdesc = new int[2];
 //
 //        P5.pipe.close(pdesc[0]);
@@ -100,9 +83,5 @@ public class Main {
 //        PCB P7=PM.fork(PM.init,"P7",120,"");
 //        System.out.println("Drzewo procesow:");
 //        PM.showTree(PM.init);
-        //PM.kill(P5);
-        //System.out.println("\nDrzewo procesow:");
-        //PM.showTree(PM.init);
-        //PM.showAllProcesses();
     }
 }
