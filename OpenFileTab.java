ea3ebcf0897222cde5930e8a1015335f4b999882
Dzemoro <56439030+Dzemoro@users.noreply.github.com>
2020-01-08 11:02:49 +0100

    Add files via upload
    tabliczka

---
+++ OpenFileTab.java
@@ -0,0 +1,31 @@
+package com.poznan.put;
+
+import java.util.Hashtable;
+
+public class OpenFileTab
+{
+    public Hashtable<PCB, File> tab;
+
+    public OpenFileTab()
+    {
+        tab = new Hashtable<PCB, File>();
+    }
+
+    public void addFile(PCB pcb ,File file)
+    {
+        int num = file.number;
+        Inode[] inodeTab = Disc.getInodeTableInstance();
+        inodeTab[num].state = true;
+        inodeTab[num].lock.acquire(pcb);
+        tab.put(pcb, file);
+    }
+
+    public void removeFile(PCB pcb, File file)
+    {
+        int num = file.number;
+        Inode[] inodeTab = Disc.getInodeTableInstance();
+        inodeTab[num].state = false;
+        inodeTab[num].lock.release(pcb);
+        tab.remove(pcb);
+    }
+}
