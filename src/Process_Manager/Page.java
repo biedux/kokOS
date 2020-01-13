7e2b67dd5470f4e7f269e683bc67005c02d74551
Dami46 <56515672+Dami46@users.noreply.github.com>
2020-01-13 18:28:27 +0100

    Update Page.java

--- src/Process_Manager/Page.java
+++ src/Process_Manager/Page.java
@@ -1,12 +1,14 @@
 package VirtualMemory;
 
 // pojedynczy wpis w tablicy stronic
-public class Page {
 
+public class Page {
+    boolean odniesienia;
     boolean valid;
     int nrramki;
     Page()
     {
+        odniesienia = false;
         valid = false;
         nrramki = -1;
     }
