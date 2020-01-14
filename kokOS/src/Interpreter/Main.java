7b2108e2cac105ea0497490672d0f8c6b9358eba
CiechanM <56478475+CiechanM@users.noreply.github.com>
2020-01-14 15:44:43 +0100

    dodano logo systemu

--- kokOS/src/Interpreter/Main.java
+++ kokOS/src/Interpreter/Main.java
@@ -15,6 +15,51 @@ public class Main {
         shell.start();
     }
 }
+
+/*
+        System.out.println("                                                            \n" +
+                "                                           ```              \n" +
+                "                           `..         `:osys-              \n" +
+                "                            :sys+:`   -+sso:                \n" +
+                "                             `/syys:`/oso:``...`            \n" +
+                "                          ``````:sso/+ss+:ossyyyso/-        \n" +
+                "                      -/+osoooo//:+soooooooo++/::-.`        \n" +
+                "                    `-......--:/++ooooooooosoo++:.          \n" +
+                "                          `..:/+++++o++++++:-:/+oso.        \n" +
+                "                    `.` `-///:-:+/+++++yo+++/.    .-        \n" +
+                "         `/ss+-   :oo-  ./+:-``:/+++osys+-+++/.             \n" +
+                "         ` `:oo+-oo::/oo+++/` .+ooo/.-`   `/ooo.            \n" +
+                "     .:/oo+++:osoo++/:. `o+-  .oso:        `+ss:            \n" +
+                "        .-/++++ooo++///-.-+   `os+`         `os:            \n" +
+                "      .+o++//+/+++so.-/+o+-   `+o`           -s`            \n" +
+                "     -so/+/-`/++//o.   `-os- `/+-             -             \n" +
+                "     +- :+:  :s+` `/:     -+`-+/                            \n" +
+                "        .+`  `/-   `:/      `/o.                            \n" +
+                "         `          `:/     -+/                             \n" +
+                "                     `//   `/+-                             \n" +
+                "                      `+-  .+o.                             \n" +
+                "                       -+. :++`                             \n" +
+                "                        //`/+s-                             \n" +
+                "                        -+-/ss.                             \n" +
+                "                        -o+oso/:                            \n" +
+                "                      .:/sosssso/`                          \n" +
+                "                    `:/ossssssss+-`                         \n" +
+                "            ````....-----------------.....```               ");
+
+        System.out.println("oooo                  oooo          .oooooo.    .oooooo..o \n" +
+                "`888                  `888         d8P'  `Y8b  d8P'    `Y8 \n" +
+                " 888  oooo   .ooooo.   888  oooo  888      888 Y88bo.      \n" +
+                " 888 .8P'   d88' `88b  888 .8P'   888      888  `\"Y8888o.  \n" +
+                " 888888.    888   888  888888.    888      888      `\"Y88b \n" +
+                " 888 `88b.  888   888  888 `88b.  `88b    d88' oo     .d8P \n" +
+                "o888o o888o `Y8bod8P' o888o o888o  `Y8bood8P'  8\"\"88888P'  ");
+*/
+
+
+
+
+
+
         //  Scan();
 //        int[] pdesc = new int[2];
 //        IPC a = new IPC();
